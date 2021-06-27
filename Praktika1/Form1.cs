49fd015a56471595ad11bf27bfe64d9785b35924
AlexandrShmelev320 <Shmeliov.135@mail.ru>
2021-06-27 21:36:17 +0500

    Добавьте файлы проекта.

---
+++ Praktika1/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Praktika1
+{
+    public partial class Form1 : Form
+    {
+        Bitmap pic;
+        Bitmap pic1;
+        string mode;
+        int x1, y1;
+        int xclick, yclick;
+        public Form1()
+        {
+            mode = "Линия";
+            InitializeComponent();
+            pic = new Bitmap(1000, 1000);
+            pic1 = new Bitmap(1000, 1000);
+            SolidBrush b = new SolidBrush(Color.White);
+            Graphics.FromImage(pic).FillRectangle(b, 0, 0, pic.Width, pic.Height);
+            Graphics.FromImage(pic1).FillRectangle(b, 0, 0, pic1.Width, pic1.Height);
+            x1 = y1 = 0;
+            pictureBox1.Image = pic;
+        }
+    }
+}
